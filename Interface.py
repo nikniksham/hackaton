21f39fc906718cc8514c0785d1c3ee9244ebb37c
nikniksham <nikniksham@gmail.com>
2019-12-08 17:05:36 +0300

    тестовая рабочая версия поиска по тестовой db

--- Interface.py
+++ Interface.py
@@ -1,5 +1,7 @@
-import sqlite3
 import sys
+import sqlite3
+from xlsxwriter.workbook import Workbook
+from openpyxl import load_workbook
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem
 
@@ -12,6 +14,8 @@ class EntrepreneurDB:
 class Search(QWidget):
     def __init__(self):
         super().__init__()
+        self.request = f'''SELECT * FROM Films
+                                WHERE year >= 0'''
         self.db = EntrepreneurDB()
         uic.loadUi('Interface_program.ui', self)
         self.characteristic = [self.surname, self.name, self.patronymic, self.name_company, self.INN, self.OGRN,
@@ -28,41 +32,46 @@ class Search(QWidget):
         self.test_characteristic = [self.surname, self.name, self.patronymic, self.name_company, self.INN]
         self.test_dictionary_characteristic = {'id': '', 'title': '', 'year': '', 'genre': '', 'duration': ''}
         self.Search.clicked.connect(self.search)
+        self.Save_to_excel.clicked.connect(self.save_to_excel)
 
     def search(self):
-        # создаём запрос
         i = 0
         for name in self.test_characteristic:
             self.test_dictionary_characteristic[self.test_name_s[i]] = name.text()
             if name.text() == '':
                 self.test_dictionary_characteristic[self.test_name_s[i]] = ''
             i += 1
-        request = f'''SELECT * FROM Films
+        self.request = f'''SELECT * FROM Films
                         WHERE year >= 0'''
         for name in self.test_name_s:
             if self.test_dictionary_characteristic[name] != '':
-                request += f''' and {name} like "%{self.test_dictionary_characteristic[name]}%"'''
-        # Подключение к БД
+                self.request += f''' and {name} like "%{self.test_dictionary_characteristic[name]}%"'''
         con = sqlite3.connect(self.db.location)
-        # Создание курсора
         cur = con.cursor()
-        # Выполнение запроса и получение всех результатов
-        titles = cur.execute(request).description
+        titles = cur.execute(self.request).description
         titles = [elem[0] for elem in titles]
-        result = cur.execute(request).fetchall()
-        # закрываем базу данных
+        result = cur.execute(self.request).fetchall()
         con.close()
-        # задаём ширину таблицы
         self.table.setColumnCount(len(titles))
-        # задаём заголовки столбцов
         self.table.setHorizontalHeaderLabels(titles)
-        # задаём количество строк
         self.table.setRowCount(len(result))
-        # задаём значения строкам
         for i, row in enumerate(result):
             for j, elem in enumerate(row):
                 self.table.setItem(i, j, QTableWidgetItem(str(elem)))
         self.table.resizeColumnsToContents()
+
+    def save_to_excel(self):
+        workbook = Workbook('excel.xlsx')
+        worksheet = workbook.add_worksheet()
+
+        con = sqlite3.connect(self.db.location)
+        cur = con.cursor()
+        result = cur.execute(self.request).fetchall()
+        con.close()
+        for i, row in enumerate(result):
+            for j, elem in enumerate(row):
+                worksheet.write(i, j, row[j])
+        workbook.close()
 
 
 app = QApplication(sys.argv)
